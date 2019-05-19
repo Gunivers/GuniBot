979776ca3fd9fd1fa2debbfbcbc9299681e9efb9
theogiraudet <theo.giraudet@orange.fr>
2019-05-19 16:59:43 +0200

    Up

---
+++ src/main/java/net/gunivers/gunibot/commands/HelloWord.java
@@ -0,0 +1,10 @@
+package net.gunivers.gunibot.commands;
+
+@Ignore
+public class HelloWord extends Command {
+
+	public HelloWord() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/net/gunivers/gunibot/commands/test/Foo.java
@@ -0,0 +1,11 @@
+package net.gunivers.gunibot.commands.test;
+
+import net.gunivers.gunibot.commands.Command;
+
+public class Foo extends Command {
+
+	public Foo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
