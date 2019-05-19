f4337340ec5066fbf85c80d9a7ca2d9f3ac9221e
Syl2010 <sylvain.meunier13@gmail.com>
2019-05-19 17:21:58 +0200

    add DataGuild

--- src/main/java/net/gunivers/gunibot/DataCenter.java
+++ src/main/java/net/gunivers/gunibot/DataCenter.java
@@ -1,17 +1,28 @@
 package net.gunivers.gunibot;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.Guild;
 import discord4j.core.object.presence.Activity;
 import discord4j.core.object.presence.Presence;
+import discord4j.core.object.util.Snowflake;
 
 public class DataCenter {
 
 	private DiscordClient botClient;
+	private ConcurrentHashMap<Snowflake, DataGuild> dataGuilds;
 
 	public DataCenter(ReadyEvent event) {
 		botClient = event.getClient();
 		botClient.updatePresence(Presence.idle(Activity.watching("Loading Data Control..."))).subscribe();
+
+		dataGuilds = new ConcurrentHashMap<>();
+	}
+
+	public void addGuild(Guild guild) {
+		dataGuilds.put(guild.getId(), new DataGuild(guild));
 	}
 
 }
---
+++ src/main/java/net/gunivers/gunibot/DataGuild.java
@@ -0,0 +1,13 @@
+package net.gunivers.gunibot;
+
+import discord4j.core.object.entity.Guild;
+
+public class DataGuild {
+
+	Guild guild;
+
+	public DataGuild(Guild guild) {
+		this.guild = guild;
+	}
+
+}
