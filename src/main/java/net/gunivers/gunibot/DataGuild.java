ecb9618962c5f17199c58d7c8e7ce535e2965546
A:Z <justin.carel@free.fr>
2019-05-24 19:31:00 +0200

    Add org.json + backup command json

--- src/main/java/net/gunivers/gunibot/DataGuild.java
+++ src/main/java/net/gunivers/gunibot/DataGuild.java
@@ -2,12 +2,14 @@ package net.gunivers.gunibot;
 
 import discord4j.core.object.entity.Guild;
 
-public class DataGuild {
-
+public class DataGuild
+{
 	Guild guild;
 
-	public DataGuild(Guild guild) {
+	public DataGuild(Guild guild)
+	{
 		this.guild = guild;
 	}
-
+	
+	
 }
