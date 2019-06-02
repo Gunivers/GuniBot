18aee3683cd32041c2b2cb4443209795a4bd0983
A:Z <justin.carel@free.fr>
2019-06-02 21:20:48 +0200

    Le push de la batterie faible

--- src/main/java/net/gunivers/gunibot/command/commands/HelpCommand.java
+++ src/main/java/net/gunivers/gunibot/command/commands/HelpCommand.java
@@ -16,36 +16,43 @@ public class HelpCommand extends Command
 	{
 		event.getMessage().getChannel().flatMap(channel -> channel.createEmbed(embed -> 
 		{
-			Util.formatEmbed(event, "**Help Menu**", embed);
+			Util.formatEmbed(event, "Help Menu", embed);
 
 			//Field: command list
 			StringBuilder sb = new StringBuilder();
-			Command.commands.keySet().forEach(aliases -> sb.append(aliases.subList(1, aliases.size() -2).stream()
-				.reduce(" - "+ aliases.get(0) + (aliases.size() == 1 ? "" : " ["), (r,s) -> r += s + ", ")
-					+ (aliases.size() == 1 ? "" : aliases.get(aliases.size() -1) + "]\n")));
+			for (List<String> aliases : Command.commands.keySet())
+			{
+				for (String alias : aliases)
+				{
+					if (alias == aliases.get(0)) sb.append(" - " + alias + (aliases.size() > 1 ? " [" : ""));
+					else sb.append(alias);
+					
+					if (aliases.size() > 1 && alias == aliases.get(aliases.size() -1)) sb.append("]\n");
+					else if (alias != aliases.get(0)) sb.append(", ");
+				}
+			}
 
-			embed.addField("**Commands List**", sb.toString(), false);
-			
-			
-			//
+			embed.addField("Commands List", sb.toString(), false);
 		})).subscribe();
 	}
 
 	public void getHelp(MessageCreateEvent event, List<String> args)
 	{
 		Command cmd = null;
-		for (Entry<List<String>, Command> entry : commands.entrySet())
-			if (entry.getKey().contains(args.get(0))) cmd = entry.getValue();
+		for (Entry<List<String>, Command> entry : commands.entrySet()) if (entry.getKey().contains(args.get(0))) cmd = entry.getValue();
 		
 		if (cmd == null)
+		{
 			event.getMessage().getChannel().flatMap(channel ->
-			channel.createMessage("```\n❌ There is no command named"+ args.get(0) +"```")).subscribe();
+				channel.createMessage("```\n❌ There is no such command as "+ args.get(0) +"```")).subscribe();
+			return;
+		}
 		
 		try { cmd.getClass().getMethod("help", MessageCreateEvent.class).invoke(cmd, event); }
 		catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e)
 		{
 			event.getMessage().getChannel().flatMap(channel ->
-			channel.createMessage("```\n❌ Unable to retrieve help from command "+ args.get(0) +"```"));
+				channel.createMessage("```\n❌ Unable to retrieve help from command "+ args.get(0) +"```")).subscribe();
 		}
 	}
 }
