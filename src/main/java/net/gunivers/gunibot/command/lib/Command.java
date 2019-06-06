cf9ceb439d396c88121bd4874b12c68d83d13274
theogiraudet <theo.giraudet@orange.fr>
2019-06-06 17:27:33 +0200

    Permission

--- src/main/java/net/gunivers/gunibot/command/lib/Command.java
+++ src/main/java/net/gunivers/gunibot/command/lib/Command.java
@@ -2,7 +2,6 @@ package net.gunivers.gunibot.command.lib;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -14,44 +13,40 @@ import org.reflections.Reflections;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import net.gunivers.gunibot.command.lib.nodes.Node;
-import net.gunivers.gunibot.command.lib.nodes.TypeNode;
-import net.gunivers.gunibot.command.permissions.Permission;
-import net.gunivers.gunibot.command.lib.annotations.Ignore;
-import net.gunivers.gunibot.command.lib.annotations.KeepAsList;
-import net.gunivers.gunibot.command.lib.nodes.ListNode;
+import net.gunivers.gunibot.command.lib.nodes.NodeList;
 import net.gunivers.gunibot.utils.tuple.Tuple2;
 
 public abstract class Command {
-    
-    public static final String PREFIX = "/";
-    public static final Map<List<String>, Command> commands = new HashMap<>();
-    
-    private String description = "";
-    private Node syntax = null;
-    private Set<Permission> permissions = new HashSet<>();
-    private Set<String> aliases = new HashSet<>();
+	
+	public static final String PREFIX = "/";
+	public static final Map<List<String>, Command> commands = new HashMap<>();
+	
+	private String description = "";
+	private Node syntax = null;
+	private Set<Permission> permissions = new HashSet<>();
+	private Set<String> aliases = new HashSet<>();
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public Set<Permission> getPermissions() {
-        return permissions;
-    }
+	 public Set<Permission> getPermissions() {
+	        return permissions;
+	    }
 
-    public void addPermissions(List<String> permissions)
-    {
-        for (String perm : permissions)
-        {
-            List<Permission> perms = Permission.getByName(perm);
-            if (perms.isEmpty()) throw new NullPointerException("Permission '" + perm + "' doesn't exist");
-            this.permissions.addAll(perms);
-        }
-    }
+	  public void addPermissions(List<String> permissions)
+	    {
+	        for (String perm : permissions)
+	        {
+	            List<Permission> perms = Permission.getByName(perm);
+	            if (perms.isEmpty()) throw new NullPointerException("Permission '" + perm + "' doesn't exist");
+	            this.permissions.addAll(perms);
+	        }
+	    }
 
 	public Set<String> getAliases() {
 		return aliases;
@@ -65,34 +60,20 @@ public abstract class Command {
 		syntax = n;
 	}
 	
-	@SuppressWarnings({ "serial", "rawtypes" })
 	public void apply(String[] command, MessageCreateEvent event)
 	{
-		Tuple2<Tuple2<Map<TypeNode,Object>, Method>, CommandSyntaxError> result = syntax.parseCommand(event, command);
-		
+		Tuple2<Tuple2<List<String>, Method>, CommandSyntaxError> result = syntax.matches(command);
 		if(result._1 != null)
 		{
 			try
 			{
-				if(result._1._1.size() > 0)	
-				{
-					if (result._1._2.isAnnotationPresent(KeepAsList.class))
-					{
-						result._1._1.replaceAll(TypeNode<Object>::parse);
-						result._1._2.invoke(this, event, new ArrayList<>(result._1._1.values()));
-					}
-					else
-						result._1._2.invoke(this, new HashMap<TypeNode,Object>() {{put(null, event); putAll(result._1._1);}}.values().toArray());
-				}
+				if(result._1._1.size() > 0)
+				result._1._2.invoke(this, event, result._1._1);
 				else
 					result._1._2.invoke(this, event);
 			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
 			{
 				e.printStackTrace();
-				event.getMessage().getChannel().flatMap(channel -> channel.createMessage(
-						"```❌  An error occured while running the command: " + e.getClass().getSimpleName() + ": " + e.getMessage() + "```"
-						+ "\nIf it persists please contact this bot developpers on Gunivers"
-						+ "\n||<https://discord.gg/EncRXj2>||")).subscribe();
 			}
 		} else
 		{
@@ -115,7 +96,7 @@ public abstract class Command {
 			try {
 				if(!cmd.isAnnotationPresent(Ignore.class)) {
 					Command c = cmd.newInstance();
-					ListNode<String> n = (ListNode<String>)CommandParser.parseCommand(c);
+					NodeList<String> n = (NodeList<String>)CommandParser.parseCommand(c);
 					List<String> aliases = n.getElements();
 					System.out.println(c.toString());
 //					Function.functions.put(aliases.get(0), n);
