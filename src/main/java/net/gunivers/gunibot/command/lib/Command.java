59155576d448807f4cf82780a935c0a132f06d80
theogiraudet <theo.giraudet@orange.fr>
2019-06-06 17:27:33 +0200

    Permission

--- src/main/java/net/gunivers/gunibot/command/lib/Command.java
+++ src/main/java/net/gunivers/gunibot/command/lib/Command.java
@@ -2,6 +2,8 @@ package net.gunivers.gunibot.command.lib;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -12,15 +14,15 @@ import java.util.stream.Collectors;
 import org.reflections.Reflections;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import net.gunivers.gunibot.command.lib.nodes.ListNode;
 import net.gunivers.gunibot.command.lib.nodes.Node;
-import net.gunivers.gunibot.command.lib.nodes.NodeList;
 import net.gunivers.gunibot.utils.tuple.Tuple2;
 
 public abstract class Command {
-	
+
 	public static final String PREFIX = "/";
 	public static final Map<List<String>, Command> commands = new HashMap<>();
-	
+
 	private String description = "";
 	private Node syntax = null;
 	private Set<String> permissions = new HashSet<>();
@@ -49,51 +51,61 @@ public abstract class Command {
 	public void setAliases(String alias) {
 		aliases.add(alias);
 	}
-	
+
 	public void setSyntax(Node n) {
 		syntax = n;
 	}
-	
+
+	@SuppressWarnings("serial")
 	public void apply(String[] command, MessageCreateEvent event)
 	{
-		Tuple2<Tuple2<List<String>, Method>, CommandSyntaxError> result = syntax.matches(command);
+		Tuple2<Tuple2<List<Object>, Method>, CommandSyntaxError> result = syntax.matches(event.getGuild().block(), command);
 		if(result._1 != null)
 		{
 			try
 			{
 				if(result._1._1.size() > 0)
-				result._1._2.invoke(this, event, result._1._1);
+				{
+					System.out.println(new ArrayList<Object>() {{ addAll(result._1._1); }}.stream().reduce("[", (r,s) -> r += s.getClass().getSimpleName() + ' '));
+					System.out.println(Arrays.asList(result._1._2));
+					result._1._2.invoke(this, new ArrayList<Object>() {{ add(event); addAll(result._1._1); }}.toArray());
+				}
 				else
 					result._1._2.invoke(this, event);
 			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
 			{
 				e.printStackTrace();
+
+				event.getMessage().getChannel().flatMap(channel -> channel.createMessage(
+						"```❌  An error occured while running the command: " + e.getMessage() + "```"
+								+ "\nIf it persists please contact this bot developpers on Gunivers"
+								+ "\n||<https://discord.gg/EncRXj2>||")).subscribe();
 			}
 		} else
 		{
 			List<String> list = result._2.getPath();
 			list.set(list.size() - 1, "__" + list.get(list.size() - 1) + "__");
 			event.getMessage().getChannel().block().createMessage("Une erreur a été détectée ici : " + list.stream().collect(Collectors.joining(" ")) + '\n' + result._2).subscribe();
-//			System.out.println("Une erreur a été détectée ici : " + result._2.getPath().stream().collect(Collectors.joining(" ")));
+			//			System.out.println("Une erreur a été détectée ici : " + result._2.getPath().stream().collect(Collectors.joining(" ")));
 		}
 	}
 
 	public abstract String getSyntaxFile();
-	
+
 	/**
 	 * Permet de charger toutes les commandes du Package net.gunivers.gunibot.command.commands
 	 */
 	public static void loadCommands() {
 		Reflections reflections = new Reflections("net.gunivers.gunibot.command.commands");
-		 Set<Class<? extends Command>> allCommands = reflections.getSubTypesOf(Command.class);
-		 allCommands.forEach(cmd -> {
+		Set<Class<? extends Command>> allCommands = reflections.getSubTypesOf(Command.class);
+		allCommands.forEach(cmd -> {
 			try {
 				if(!cmd.isAnnotationPresent(Ignore.class)) {
 					Command c = cmd.newInstance();
-					NodeList<String> n = (NodeList<String>)CommandParser.parseCommand(c);
+					ListNode<String> n = (ListNode<String>)CommandParser.parseCommand(c);
 					List<String> aliases = n.getElements();
 					System.out.println(c.toString());
-//					Function.functions.put(aliases.get(0), n);
+					//					Function.functions.put(aliases.get(0), n);
 					commands.put(aliases, c);
 				}
 			} catch (InstantiationException | IllegalAccessException e) {
@@ -101,7 +113,7 @@ public abstract class Command {
 			}
 		});
 	}
-	
+
 	@Override
 	public String toString() {
 		return Command.PREFIX + syntax.toString();
