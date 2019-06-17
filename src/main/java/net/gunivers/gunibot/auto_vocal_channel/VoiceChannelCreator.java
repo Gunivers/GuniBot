0d3e37c1a76cd7a6bbda9a95d0da668c211f1a06
theogiraudet <theo.giraudet@orange.fr>
2019-06-17 23:07:02 +0200

    Ajout Autochannel système

---
+++ src/main/java/net/gunivers/gunibot/auto_vocal_channel/VoiceChannelCreator.java
@@ -0,0 +1,117 @@
+package net.gunivers.gunibot.auto_vocal_channel;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.event.domain.VoiceStateUpdateEvent;
+import discord4j.core.object.PermissionOverwrite;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.VoiceChannel;
+import discord4j.core.object.util.Permission;
+import discord4j.core.object.util.PermissionSet;
+import discord4j.core.object.util.Snowflake;
+import reactor.core.Disposable;
+import reactor.core.publisher.Mono;
+
+public class VoiceChannelCreator {
+
+	private static Set<Snowflake> voiceChannels = new HashSet<>();
+	private static Map<Snowflake, Snowflake> createdChannels = new HashMap<>();
+	private static Map<Snowflake, Disposable> removingChannels = new HashMap<>();
+	private static List<String> names = new ArrayList<>();
+	
+	private final static int DELETE_DELAY = 300000; 
+	
+	public static void addVoiceChannelCreator(VoiceChannel... voiceChannel) {
+		voiceChannels.addAll(Arrays.asList(voiceChannel).stream().map(vc -> vc.getId()).collect(Collectors.toList()));
+	}
+	
+	public static boolean isVoiceChannelCreator(VoiceChannel voiceChannel) {
+		return voiceChannels.contains(voiceChannel.getId());
+	}
+	
+	public static void removeVoiceChannelCreator(VoiceChannel... voiceChannel) {
+		voiceChannels.removeAll(Arrays.asList(voiceChannel).stream().map(vc -> vc.getId()).collect(Collectors.toList()));
+	}
+	
+	public static Set<Snowflake> getVoiceChannelCreator() {
+		return new HashSet<>(voiceChannels);
+	}
+	
+	public static void init(DiscordClient dc) {
+		try {
+			names = Files.lines(Paths.get(VoiceChannelCreator.class.getResource("/other/bdd name").toURI())).collect(Collectors.toList());
+			Collections.shuffle(names);
+		} catch (IOException | URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+		
+		//On vérifie s'il s'agit d'une connexion, si oui, on ne garde que les salons définis comme étant Creator
+		dc.getEventDispatcher().on(VoiceStateUpdateEvent.class)
+			.filter(e -> e.getCurrent().getChannelId().isPresent() && voiceChannels.contains(e.getCurrent().getChannelId().get()))
+			.subscribe(e -> joinEvent(e.getCurrent().getUser().block().asMember(e.getCurrent().getGuildId()).block(), e.getCurrent().getChannel().block()));
+		
+		//On vérifie s'il s'agit d'une connexion, si oui, on ne garde que les salons définis comme étant créés et étant en cours de suppression
+		dc.getEventDispatcher().on(VoiceStateUpdateEvent.class)
+				.filter(e -> e.getCurrent().getChannelId().isPresent() && createdChannels.containsValue(e.getCurrent().getChannelId().get()) && removingChannels.containsKey(e.getCurrent().getChannelId().get()))
+				.subscribe(e -> joinDeletingEvent(e.getCurrent().getChannelId().get()));
+		
+		//On vérifie s'il s'agit d'une déconnexion, si oui, on ne garde que les salons définis comme étant créés par un salon
+		dc.getEventDispatcher().on(VoiceStateUpdateEvent.class)
+			.filter(e -> e.getOld().isPresent() && e.getOld().get().getChannelId().isPresent() && createdChannels.containsValue(e.getOld().get().getChannelId().get()))
+			.subscribe(e -> leaveEvent(e.getOld().get().getChannel().block()));
+	}
+
+	
+	private static void joinEvent(Member e, VoiceChannel vc) {
+		if(createdChannels.containsKey(e.getId()))
+			e.edit(gmes -> gmes.setNewVoiceChannel(createdChannels.get(e.getId()))).subscribe();
+		else {
+			VoiceChannel vc2 = vc.getGuild().block().createVoiceChannel(vccs -> 
+			{ 
+				Random r = new Random();
+				vccs.setName(names.get(r.nextInt(names.size()))); 
+				vccs.setParentId(vc.getCategoryId().get()); 
+				vccs.setPosition(vc.getRawPosition() + 1); 
+				Set<PermissionOverwrite> set = new HashSet<>();
+				set.add(PermissionOverwrite.forMember(e.getId(), PermissionSet.of(Permission.MANAGE_CHANNELS, Permission.MANAGE_ROLES), PermissionSet.none()));
+				vccs.setPermissionOverwrites(set);
+			}
+			).block();
+			e.edit(gmes -> gmes.setNewVoiceChannel(vc2.getId())).subscribe();
+			createdChannels.put(e.getId(), vc2.getId());
+		}
+	}
+	
+	private static void joinDeletingEvent(Snowflake sf) {
+		removingChannels.get(sf).dispose();
+		removingChannels.remove(sf);
+	}
+	
+	private static void leaveEvent(VoiceChannel vc) {
+		if(vc.getVoiceStates().count().block().intValue() == 0) {
+			try {
+				Mono<VoiceChannel> mono = Mono.just(vc).delayElement(Duration.ofMillis(DELETE_DELAY));
+				Disposable disposal = mono.subscribe(s -> s.delete().subscribe());
+				removingChannels.put(vc.getId(), disposal);
+				Thread.sleep(DELETE_DELAY);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/main/java/net/gunivers/gunibot/command/commands/AutoChannelCommand.java
@@ -0,0 +1,52 @@
+package net.gunivers.gunibot.command.commands;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.GuildChannel;
+import discord4j.core.object.entity.VoiceChannel;
+import discord4j.core.object.util.Snowflake;
+import net.gunivers.gunibot.auto_vocal_channel.VoiceChannelCreator;
+import net.gunivers.gunibot.core.az.lib.EmbedBuilder;
+import net.gunivers.gunibot.core.az.lib.EmbedBuilder.Field;
+import net.gunivers.gunibot.core.command.Command;
+
+public class AutoChannelCommand extends Command {
+	
+	public void registerChannel(MessageCreateEvent e, List<String> args) {
+		try {
+			GuildChannel sf = e.getGuild().block().getChannelById(Snowflake.of(args.get(0))).block();
+			if(VoiceChannelCreator.isVoiceChannelCreator((VoiceChannel)sf)) {
+				VoiceChannelCreator.removeVoiceChannelCreator((VoiceChannel)sf);
+				e.getMessage().getChannel().block().createMessage(((VoiceChannel)sf).getName() + " is no longer an Auto Channel!").subscribe();
+			} else {
+				VoiceChannelCreator.addVoiceChannelCreator((VoiceChannel)sf);
+				e.getMessage().getChannel().block().createMessage(((VoiceChannel)sf).getName() + " is now an Auto Channel!").subscribe();
+			}
+		} catch(Exception exc) {
+			e.getMessage().getChannel().block().createMessage("Snowflake not valid!").subscribe();
+		}
+	}
+	
+	public void list(MessageCreateEvent e) {
+		final EmbedBuilder builder = new EmbedBuilder(e, "Autochannel List", null);
+		builder.setAuthor(e.getMember().get());
+		final Field list = new Field("");
+		list.getValue().append(
+				VoiceChannelCreator.getVoiceChannelCreator()
+				.stream().map(s -> e.getGuild().block().getChannelById(s).doOnError(null).block())
+				.filter(c -> c != null && c instanceof VoiceChannel)
+				.map(c -> c.getName())
+				.collect(Collectors.joining("\n"))
+		);
+		builder.addField(list);
+		builder.buildAndSend();
+	}
+
+	@Override
+	public String getSyntaxFile() {
+		return "autochannel.json";
+	}
+
+}
