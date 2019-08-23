83daee0080d0327d960f16d5a2828994de15e67e
Syl2010 <sylvain.meunier13@gmail.com>
2019-08-23 04:59:24 +0200

    DataGuild genericity simplification

--- src/main/java/net/gunivers/gunibot/datas/DataGuild.java
+++ src/main/java/net/gunivers/gunibot/datas/DataGuild.java
@@ -2,14 +2,16 @@ package net.gunivers.gunibot.datas;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map.Entry;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 import org.json.JSONObject;
 
 import discord4j.core.object.entity.Category;
+import discord4j.core.object.entity.Entity;
 import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Role;
@@ -17,6 +19,7 @@ import discord4j.core.object.entity.TextChannel;
 import discord4j.core.object.entity.VoiceChannel;
 import discord4j.core.object.util.Snowflake;
 import net.gunivers.gunibot.core.utils.BotUtils;
+import reactor.core.publisher.Mono;
 
 /**
  * Classe de donnée pour les objets Guild.
@@ -55,19 +58,32 @@ public class DataGuild extends DataObject<Guild>
 	}
 
 	/**
+	 * Récupère et/ou enregistre un DataObject à partir de la map de donnée, du constructeur, et de l'entité en paramètre
+	 * @param <D> le type de DataObject
+	 * @param <E> le type de l'entité (équivalent au type du DataObject)
+	 * @param data_map la map de donnée contenant ou enregistrant le DataObject
+	 * @param data_constructor le constructeur permettant de généré ce DataObject
+	 * @param entity l'entité lié au DataObject à récupéré
+	 * @return le DataObject récupéré à partir de l'entité
+	 */
+	private static <D extends DataObject<E>,E extends Entity> D createDataObject(Map<Snowflake,D> data_map, Function<E,D> data_constructor, E entity) {
+		D data_object = data_map.get(entity.getId());
+		if(data_object != null) {
+			return data_object;
+		} else {
+			data_object = data_constructor.apply(entity);
+			data_map.put(entity.getId(), data_object);
+			return data_object;
+		}
+	}
+
+	/**
 	 * Créer et/ou récupère les données du membre indiqué en cache dans cet objet.
 	 * @param member le membre.
 	 * @return l'objet de donnée du membre.
 	 */
 	public DataMember getDataMember(Member member) {
-		DataMember data_member = dataMembers.get(member.getId());
-		if(data_member != null) {
-			return data_member;
-		} else {
-			data_member = new DataMember(member);
-			dataMembers.put(member.getId(), data_member);
-			return data_member;
-		}
+		return createDataObject(dataMembers, DataMember::new, member);
 	}
 
 	/**
@@ -76,14 +92,7 @@ public class DataGuild extends DataObject<Guild>
 	 * @return l'objet de donnée du channel textuel.
 	 */
 	public DataTextChannel getDataTextChannel(TextChannel text_channel) {
-		DataTextChannel data_text_channel = dataTextChannels.get(text_channel.getId());
-		if(data_text_channel != null) {
-			return data_text_channel;
-		} else {
-			data_text_channel = new DataTextChannel(text_channel);
-			dataTextChannels.put(text_channel.getId(), data_text_channel);
-			return data_text_channel;
-		}
+		return createDataObject(dataTextChannels, DataTextChannel::new, text_channel);
 	}
 
 	/**
@@ -92,14 +101,7 @@ public class DataGuild extends DataObject<Guild>
 	 * @return l'objet de donnée du role.
 	 */
 	public DataRole getDataRole(Role role) {
-		DataRole data_role = dataRoles.get(role.getId());
-		if(data_role != null) {
-			return data_role;
-		} else {
-			data_role = new DataRole(role);
-			dataRoles.put(role.getId(), data_role);
-			return data_role;
-		}
+		return createDataObject(dataRoles, DataRole::new, role);
 	}
 
 	/**
@@ -108,14 +110,7 @@ public class DataGuild extends DataObject<Guild>
 	 * @return l'objet de donnée du channel vocal.
 	 */
 	public DataVoiceChannel getDataVoiceChannel(VoiceChannel voice_channel) {
-		DataVoiceChannel data_voice_channel = dataVoiceChannels.get(voice_channel.getId());
-		if(data_voice_channel != null) {
-			return data_voice_channel;
-		} else {
-			data_voice_channel = new DataVoiceChannel(voice_channel);
-			dataVoiceChannels.put(voice_channel.getId(), data_voice_channel);
-			return data_voice_channel;
-		}
+		return createDataObject(dataVoiceChannels, DataVoiceChannel::new, voice_channel);
 	}
 
 	/**
@@ -124,14 +119,7 @@ public class DataGuild extends DataObject<Guild>
 	 * @return l'objet de donnée de la catégorie.
 	 */
 	public DataCategory getDataCategory(Category category) {
-		DataCategory data_category = dataCategories.get(category.getId());
-		if(data_category != null) {
-			return data_category;
-		} else {
-			data_category = new DataCategory(category);
-			dataCategories.put(category.getId(), data_category);
-			return data_category;
-		}
+		return createDataObject(dataCategories, DataCategory::new, category);
 	}
 
 	//	//En réflexion
@@ -164,6 +152,20 @@ public class DataGuild extends DataObject<Guild>
 	//	}
 
 	/**
+	 * Extrait les données json de tout les DataObject de la Map et les ajoute dans le JsonObject renvoyé par cette fonction,
+	 * en assignant chaque noeud json récupéré avec l'id du DataObject
+	 * @param <D> Type du DataObject
+	 * @param <E> Type de l'entité lié au DataObject
+	 * @param data_map la map de données contenant tout les DataObject d'où extraire les données Json
+	 * @return Un JSONObject contenant un noeud por chaque DataObject de la map
+	 */
+	private static <D extends DataObject<E>,E extends Entity> JSONObject extractJsonDatas(Map<Snowflake,D> data_map) {
+		JSONObject json = new JSONObject();
+		data_map.entrySet().forEach(entry -> json.put(entry.getKey().asString(), entry.getValue().save()));
+		return json;
+	}
+
+	/**
 	 * Fonction de sauvegarde de donnée appelé par DataCenter.
 	 * Les données json sont récupérés pour être enregistré dans la base de donnée.
 	 * gère également la sauvegarde des données des objets discord contenue dans cet objet.
@@ -172,35 +174,11 @@ public class DataGuild extends DataObject<Guild>
 	public JSONObject save() {
 		JSONObject json = super.save();
 
-		JSONObject json_members = new JSONObject();
-		for(Entry<Snowflake, DataMember> member_entry:dataMembers.entrySet()) {
-			json_members.putOpt(member_entry.getKey().asString(), member_entry.getValue().save());
-		}
-		json.putOpt("members", json_members);
-
-		JSONObject json_text_channels = new JSONObject();
-		for(Entry<Snowflake, DataTextChannel> text_channel_entry:dataTextChannels.entrySet()) {
-			json_text_channels.putOpt(text_channel_entry.getKey().asString(), text_channel_entry.getValue().save());
-		}
-		json.putOpt("text_channels", json_text_channels);
-
-		JSONObject json_roles = new JSONObject();
-		for(Entry<Snowflake, DataRole> roles_entry:dataRoles.entrySet()) {
-			json_roles.putOpt(roles_entry.getKey().asString(), roles_entry.getValue().save());
-		}
-		json.putOpt("roles", json_roles);
-
-		JSONObject json_voice_channels = new JSONObject();
-		for(Entry<Snowflake, DataVoiceChannel> voice_channel_entry:dataVoiceChannels.entrySet()) {
-			json_voice_channels.putOpt(voice_channel_entry.getKey().asString(), voice_channel_entry.getValue().save());
-		}
-		json.putOpt("voice_channels", json_voice_channels);
-
-		JSONObject json_categories = new JSONObject();
-		for(Entry<Snowflake, DataCategory> category_entry:dataCategories.entrySet()) {
-			json_categories.putOpt(category_entry.getKey().asString(), category_entry.getValue().save());
-		}
-		json.putOpt("categories", json_categories);
+		json.putOpt("members", extractJsonDatas(dataMembers));
+		json.putOpt("text_channels", extractJsonDatas(dataTextChannels));
+		json.putOpt("roles", extractJsonDatas(dataRoles));
+		json.putOpt("voice_channels", extractJsonDatas(dataVoiceChannels));
+		json.putOpt("categories", extractJsonDatas(dataCategories));
 
 		json.putOpt("prefix", prefix);
 
@@ -222,6 +200,39 @@ public class DataGuild extends DataObject<Guild>
 	}
 
 	/**
+	 * Créer et/ou hydrate les DataObject de la Map à partir des données json indiqués
+	 * @param <D> Type des DataObject
+	 * @param <E> Type de l'entité lié au DataObject
+	 * @param data_map la data_map à hydraté
+	 * @param data_constructor le contructeur de DataObject
+	 * @param entity_mono la fonction de récupération de l'entité
+	 * @param json les données à utiliser pour hydratés les DataObject
+	 * @param type le type/classe de l'entité
+	 */
+	private <D extends DataObject<E>,E extends Entity> void hydratateDataObject(Map<Snowflake,D> data_map, Function<E,D> data_constructor, Function<Snowflake, Mono<? super E>> entity_mono, JSONObject json, Class<E> type) {
+		if(json != null) {
+			for(String s_id:json.keySet()) {
+				Snowflake id = Snowflake.of(s_id);
+				Optional<E> opt = BotUtils.returnOptional(entity_mono.apply(id).ofType(type));
+
+				if(opt.isPresent()) {
+					if (data_map.containsKey(id)) {
+						data_map.get(id).load(json.getJSONObject(s_id));
+					} else {
+						D data_member = data_constructor.apply(opt.get());
+						data_member.load(json.getJSONObject(s_id));
+						data_map.put(id, data_member);
+					}
+				} else {
+					System.err.println(String.format("No entity '%s' with id '%s' in the guild '%s' (%s) is not reachable! Skipping loading of this entity!", type.getSimpleName(), id.asString(), getEntity().getName(), getEntity().getId().asString()));
+				}
+			}
+		} else {
+			System.out.println(String.format("No entity '%s' datas in the guild '%s' (%s)! Skipping loading of their entities!", type.getSimpleName(), getEntity().getName(), getEntity().getId().asString()));
+		}
+	}
+
+	/**
 	 * Fonction de chargement des données appelé par DataCenter.
 	 * Les données json de la base de donnée sont transmises à cette fonction.
 	 * Gère également le chargement des données des objets discords contenu dans cet objet.
@@ -233,116 +244,11 @@ public class DataGuild extends DataObject<Guild>
 		//debug
 		//System.out.println(json.toString());
 
-		JSONObject json_members = json.optJSONObject("members");
-		JSONObject json_text_channels = json.optJSONObject("text_channels");
-		JSONObject json_roles = json.optJSONObject("roles");
-		JSONObject json_voice_channels = json.optJSONObject("voice_channels");
-		JSONObject json_categories = json.optJSONObject("categories");
-
-		if(json_members != null) {
-			for(String s_member_id:json_members.keySet()) {
-				Snowflake member_id = Snowflake.of(s_member_id);
-				Optional<Member> opt_member = BotUtils.returnOptional(getEntity().getMemberById(member_id));
-
-				if(opt_member.isPresent()) {
-					if (dataMembers.containsKey(member_id)) {
-						dataMembers.get(member_id).load(json_members.getJSONObject(s_member_id));
-					} else {
-						DataMember data_member = new DataMember(opt_member.get());
-						data_member.load(json_members.getJSONObject(s_member_id));
-						dataMembers.put(member_id, data_member);
-					}
-				} else {
-					System.err.println(String.format("The member id '%s' in the guild '%s' (%s) is not reachable! Skipping loading of this member!", member_id.asString(), getEntity().getName(), getEntity().getId().asString()));
-				}
-			}
-		} else {
-			System.out.println(String.format("No members datas in the guild '%s' (%s)! Skipping loading of the members!", getEntity().getName(), getEntity().getId().asString()));
-		}
-
-		if(json_text_channels != null) {
-			for(String s_text_channel_id : json_text_channels.keySet()) {
-				Snowflake text_channel_id = Snowflake.of(s_text_channel_id);
-				Optional<TextChannel> opt_text_channel = BotUtils.returnOptional(getEntity().getChannelById(text_channel_id).ofType(TextChannel.class));
-
-				if(opt_text_channel.isPresent()) {
-					if (dataTextChannels.containsKey(text_channel_id)) {
-						dataTextChannels.get(text_channel_id).load(json_text_channels.getJSONObject(s_text_channel_id));
-					} else {
-						DataTextChannel data_text_channel = new DataTextChannel(opt_text_channel.get());
-						data_text_channel.load(json_text_channels.getJSONObject(s_text_channel_id));
-						dataTextChannels.put(text_channel_id, data_text_channel);
-					}
-				} else {
-					System.err.println(String.format("The text channel id '%s' in the guild '%s' (%s) is not reachable! Skipping loading of this text channel!", text_channel_id.asString(), getEntity().getName(), getEntity().getId().asString()));
-				}
-			}
-		} else {
-			System.out.println(String.format("No text channels datas in the guild '%s' (%s)! Skipping loading of the text channels!", getEntity().getName(), getEntity().getId().asString()));
-		}
-
-		if(json_roles != null) {
-			for(String s_role_id:json_roles.keySet()) {
-				Snowflake role_id = Snowflake.of(s_role_id);
-				Optional<Role> opt_role = BotUtils.returnOptional(getEntity().getRoleById(role_id));
-
-				if(opt_role.isPresent()) {
-					if (dataRoles.containsKey(role_id)) {
-						dataRoles.get(role_id).load(json_roles.getJSONObject(s_role_id));
-					} else {
-						DataRole data_role = new DataRole(opt_role.get());
-						data_role.load(json_roles.getJSONObject(s_role_id));
-						dataRoles.put(role_id, data_role);
-					}
-				} else {
-					System.err.println(String.format("The role id '%s' in the guild '%s' (%s) is not reachable! Skipping loading of this role!", role_id.asString(), getEntity().getName(), getEntity().getId().asString()));
-				}
-			}
-		} else {
-			System.out.println(String.format("No roles datas in the guild '%s' (%s)! Skipping loading of the roles!", getEntity().getName(), getEntity().getId().asString()));
-		}
-
-		if(json_voice_channels != null) {
-			for(String s_voice_channel_id:json_voice_channels.keySet()) {
-				Snowflake voice_channel_id = Snowflake.of(s_voice_channel_id);
-				Optional<VoiceChannel> opt_voice_channel = BotUtils.returnOptional(getEntity().getChannelById(voice_channel_id).ofType(VoiceChannel.class));
-
-				if(opt_voice_channel.isPresent()) {
-					if (dataVoiceChannels.containsKey(voice_channel_id)) {
-						dataVoiceChannels.get(voice_channel_id).load(json_voice_channels.getJSONObject(s_voice_channel_id));
-					} else {
-						DataVoiceChannel data_voice_channel = new DataVoiceChannel(opt_voice_channel.get());
-						data_voice_channel.load(json_voice_channels.getJSONObject(s_voice_channel_id));
-						dataVoiceChannels.put(voice_channel_id, data_voice_channel);
-					}
-				} else {
-					System.err.println(String.format("The voice channel id '%s' in the guild '%s' (%s) is not reachable! Skipping loading of this voice channel!", voice_channel_id.asString(), getEntity().getName(), getEntity().getId().asString()));
-				}
-			}
-		} else {
-			System.out.println(String.format("No voice channels datas in the guild '%s' (%s)! Skipping loading of the voice channels!", getEntity().getName(), getEntity().getId().asString()));
-		}
-
-		if(json_categories != null) {
-			for(String s_category_id:json_categories.keySet()) {
-				Snowflake category_id = Snowflake.of(s_category_id);
-				Optional<Category> opt_category = BotUtils.returnOptional(getEntity().getChannelById(category_id).ofType(Category.class));
-
-				if(opt_category.isPresent()) {
-					if (dataCategories.containsKey(category_id)) {
-						dataCategories.get(category_id).load(json_categories.getJSONObject(s_category_id));
-					} else {
-						DataCategory data_category = new DataCategory(opt_category.get());
-						data_category.load(json_categories.getJSONObject(s_category_id));
-						dataCategories.put(category_id, data_category);
-					}
-				} else {
-					System.err.println(String.format("The category id '%s' in the guild '%s' (%s) is not reachable! Skipping loading of this category!", category_id.asString(), getEntity().getName(), getEntity().getId().asString()));
-				}
-			}
-		} else {
-			System.out.println(String.format("No categories datas in the guild '%s' (%s)! Skipping loading of the categories!", getEntity().getName(), getEntity().getId().asString()));
-		}
+		hydratateDataObject(dataMembers, DataMember::new, getEntity()::getMemberById, json.optJSONObject("members"), Member.class);
+		hydratateDataObject(dataTextChannels, DataTextChannel::new, getEntity()::getChannelById, json.optJSONObject("text_channels"), TextChannel.class);
+		hydratateDataObject(dataRoles, DataRole::new, getEntity()::getRoleById, json.optJSONObject("roles"), Role.class);
+		hydratateDataObject(dataVoiceChannels, DataVoiceChannel::new, getEntity()::getChannelById, json.optJSONObject("voice_channels"), VoiceChannel.class);
+		hydratateDataObject(dataCategories, DataCategory::new, getEntity()::getChannelById, json.optJSONObject("categories"), Category.class);
 
 		prefix = json.optString("prefix", prefix);
 
