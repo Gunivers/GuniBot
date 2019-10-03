35bbae2156af83a21eac96c22709143079a0878a
Syl2010 <sylvain.meunier13@gmail.com>
2019-10-03 02:08:09 +0200

    optimize number reaction

--- src/main/java/net/gunivers/gunibot/core/feature/NumberToReaction.java
+++ src/main/java/net/gunivers/gunibot/core/feature/NumberToReaction.java
@@ -12,26 +12,26 @@ import net.gunivers.gunibot.core.utils.ImageUtils;
 
 public class NumberToReaction {
 
-	private final static int SIZE = 1024;
-	private final static Color COLOR = new Color(255, 87, 34);
-	private final static Font FONT = new Font("Comic Sans MS", Font.BOLD, 600);
-	
-	public static void convertNumberToReaction(int i, Message m) {
-	     
-	     BufferedImage img = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
-	     Graphics2D graphics = img.createGraphics();
-	     graphics.setColor(COLOR);
-	     graphics.fillRoundRect(0, 0, SIZE - 1, SIZE - 1, SIZE / 2, SIZE / 2);
-	     graphics.setColor(new Color(255, 255, 255));
-	     ImageUtils.drawCenterString(Integer.toString(i), FONT, graphics, SIZE, SIZE);
-	   
-	     Image image = ImageUtils.bufferedImageToByteArray(img);
-	     m.getGuild().block().createEmoji(gecs -> {
-	    	 gecs.setImage(image);
-	    	 gecs.setName("levelNumber");
-	     }).subscribe(e -> {
-	    	 m.addReaction(ReactionEmoji.custom(e)).subscribe();
-	    	 e.delete().subscribe();
-	     });
-	}
+    private final static int SIZE = 1024;
+    private final static Color COLOR = new Color(255, 87, 34);
+    private final static Font FONT = new Font("Comic Sans MS", Font.BOLD, 600);
+
+    public static void convertNumberToReaction(int i, Message message) {
+
+	BufferedImage bufferedImage = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
+	Graphics2D graphics = bufferedImage.createGraphics();
+	graphics.setColor(COLOR);
+	graphics.fillRoundRect(0, 0, SIZE - 1, SIZE - 1, SIZE / 2, SIZE / 2);
+	graphics.setColor(new Color(255, 255, 255));
+	ImageUtils.drawCenterString(Integer.toString(i), FONT, graphics, SIZE, SIZE);
+
+	Image image = ImageUtils.bufferedImageToByteArray(bufferedImage);
+	message.getGuild().flatMap(guild -> guild.createEmoji(emojiSpec -> {
+	    emojiSpec.setImage(image);
+	    emojiSpec.setName("levelNumber");
+	})).subscribe(emoji -> {
+	    message.addReaction(ReactionEmoji.custom(emoji)).subscribe();
+	    emoji.delete().subscribe();
+	});
+    }
 }
