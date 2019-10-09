751b573f916fdf4ad833c827895d06eac1fa248b
Syl2010 <sylvain.meunier13@gmail.com>
2019-10-09 23:11:28 +0200

    Fix FileSystemError

--- src/main/java/net/gunivers/gunibot/core/command/Utils.java
+++ src/main/java/net/gunivers/gunibot/core/command/Utils.java
@@ -1,22 +1,17 @@
 package net.gunivers.gunibot.core.command;
 
-import java.net.URI;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
 public class Utils {
 
-	public static String getResourceFileContent(String p, String file) {
-		try {
-			URI uri = ClassLoader.getSystemResource(p).toURI();
-			String mainPath = Paths.get(uri).toString();
-			Path path = Paths.get(mainPath, file);
-			return Files.readAllLines(path).stream().collect(Collectors.joining("\n"));
-			} catch(Exception e) {
-				e.printStackTrace();
-				return null;
-			}
+    public static String getResourceFileContent(String p, String file) {
+	try {
+	    return String.join("\n",
+		    Files.readAllLines(Paths.get(Utils.class.getClassLoader().getResource(p + "/file").toURI())));
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    return null;
 	}
+    }
 }
